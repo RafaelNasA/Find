8edc4508325d3985fbcc73d32bb07095dec36a12
rafaelnascimento <tillrafael0@gmail.com>
2023-12-16 20:28:29 -0300

    fixed:module

---
+++ BackEnd/Agencia3/src/main/java/com/agencia/agenciaAplication.java
@@ -0,0 +1,12 @@
+package com.agencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class agenciaAplication {
+	public static void main(String[] args) {
+		SpringApplication.run(agenciaAplication.class, args);
+	}
+}
